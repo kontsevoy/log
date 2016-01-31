d6d3917c1ccf030e660cb8a91b71a19faa1f8397
Ev Kontsevoy <ev@kontsevoy.com>
2016-01-31 00:22:08 -0800

    Minor changes

--- main/main.go
+++ main/main.go
@@ -11,8 +11,8 @@ func main() {
 	// use our own logger natively:
 	//slw := l.AddTarget(l.TargetSyslog, syslog.LOG_INFO)
 	//slw.SetFlags(0)
-	writer := l.AddTarget(l.TargetStdout, syslog.LOG_ERR)
-	l.SetFlags(log.LstdFlags)
+	writer := l.AddTarget(l.TargetStderr, syslog.LOG_WARNING)
+	l.SetFlags(0) //log.LstdFlags)
 
 	l.Info("This is info\n")
 	l.Warning("This is warning\n")
@@ -20,6 +20,7 @@ func main() {
 	l.Printf("This is printf %v -> %v -> %v\n\n", 1, 2, 3)
 
 	// plug our logger into standard Golang log:
+	log.SetFlags(0)
 	log.SetOutput(writer)
 	log.Println("golang log.Println")
 }
